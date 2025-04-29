148994b4757e3f6e1dc5f0b4a60471780411575e
Samuel Maiko <sammymaiko721@gmail.com>
2025-04-29 12:09:28 +0300

    removed unrequired default code ready to start

--- lib/main.dart
+++ lib/main.dart
@@ -7,147 +7,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp()    // return MaterialApp()
-
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(
-            color: Colors.green,
-            fontSize: 42,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+      title: 'Flutter Mockups',
+      theme: ThemeData(),
       initialRoute: '/',
       routes: {
-        '/': (context) => MyHomePage(title: "My routed home page"),
-        '/about': (context) => AboutPage(),
+        '/': (context) => HomePage(),
+        // '/about': (context) => AboutPage(),
       },
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  // void navigateToAboutPage() {
-  //   Navigator.pushNamed(context, '/about');
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              height: 80,
-              color: Colors.teal,
-              alignment: Alignment.center,
-              child: Text("Menu", style: TextStyle(color: Colors.white)),
-            ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text("Home"),
-              onTap: () {
-                // Navigator.pop(context);
-                Navigator.pushNamed(context, '/');
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.info),
-              title: Text("About"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/about');
-              },
-            ),
-          ],
-        ),
-      ),
-
-      // body: Container(
-      // color: Colors.blue,
-      // width: double.infinity,
-      // height: double.infinity,
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                style: TextStyle(color: Colors.white),
-                "Counter is at:",
-              ),
-              Text(
-                "$_counter",
-                style: Theme.of(
-                  context,
-                ).textTheme.headlineLarge?.copyWith(color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-      ),
-      // ),
-      // body: Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       const Text('You have pushed the button this many times:'),
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headlineMedium,
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).colorScheme.inverseSurface,
-        onPressed: _incrementCounter,
-        // onPressed: navigateToAboutPage,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add, color: Colors.white),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class AboutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("About")),
-      body: Center(child: Text('This is the about page')),
     );
   }
 }
