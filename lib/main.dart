522d5edb5bb7dd729199b1a920a70cd3e11cf518
Samuel Maiko <sammymaiko721@gmail.com>
2025-05-03 11:58:52 +0300

    removed unnecessary imports, commented of how to test a data source, removed signup option from login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:odoo_rpc/odoo_rpc.dart';
 import 'package:test_app_flutter2/bloc/login/login_bloc.dart';
 import 'package:test_app_flutter2/bloc/mtn_form/mtn_form_bloc.dart';
-import 'package:test_app_flutter2/bloc/signup/signup_bloc.dart';
 import 'package:test_app_flutter2/pages/home/home_page.dart';
 import 'package:test_app_flutter2/pages/login/login_page.dart';
 import 'package:test_app_flutter2/pages/mtn_money/mtn_money_page.dart';
-import 'package:test_app_flutter2/pages/signup/signup_page.dart';
-import 'package:http/http.dart' as http;
-import 'package:test_app_flutter2/src/data/datasources/login_remote_datasource.dart';
 import 'package:test_app_flutter2/src/data/datasources/odoo_remote_data_source.dart';
-import 'package:test_app_flutter2/src/data/repositories/login_repository_impl.dart';
 import 'package:test_app_flutter2/src/data/repositories/odoo_login_repository_impl.dart';
-import 'package:test_app_flutter2/src/domain/usecases/login_usecase.dart';
 import 'package:test_app_flutter2/src/domain/usecases/odoo_login_usecase.dart';
 
 void main() async {
+  // HOW TO TEST A DATA SOURCE
   // final dataSource = OdooRemoteDataSource("http://localhost:8001");
   // try {
   //   final session = await dataSource.login(
@@ -45,9 +39,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        // '/':
-        //     (context) =>
-        //         BlocProvider(create: (_) => SignupBloc(), child: SignupPage()),
         '/': (context) {
           // final odooClient = OdooClient('http://localhost:8001');
           final baseUrl = "http://localhost:8001";
